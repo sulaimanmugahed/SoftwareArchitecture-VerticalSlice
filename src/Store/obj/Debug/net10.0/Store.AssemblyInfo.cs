087691bdbae732002779fc448c2205cbd9035a27
sulaimanmugahed <hg123om5@gmail.com>
2026-08-20 20:23:07 +0300

    add ignore

--- src/Store/obj/Debug/net10.0/Store.AssemblyInfo.cs
+++ src/Store/obj/Debug/net10.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dedb8917817837ee74ec6ce0be280a6710acc560")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344a5ecbe5f8995305dc10336c76a58f8a7afebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
